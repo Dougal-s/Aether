68b91b9fff6fa8ec8606850a4ced0b4358de0899
Dougal Stewart <dougal.koji@gmail.com>
2021-10-24 10:14:42 +0100

    stopped explicitly setting optimization flags CMake automatically sets the appropriate flags based on the build type

--- cmake/TargetNanoVG.cmake
+++ cmake/TargetNanoVG.cmake
@@ -2,12 +2,14 @@ set(NANOVG_SOURCE_DIR "${CMAKE_SOURCE_DIR}/extern/nanovg/src")
 
 add_library(nanovg STATIC "${NANOVG_SOURCE_DIR}/nanovg.c")
 
-target_compile_options(nanovg PRIVATE -O3 -DNDEBUG)
+target_compile_definitions(nanovg
+	PUBLIC NANOVG_GL3_IMPLEMENTATION
+	PRIVATE WIN32_LEAN_AND_MEAN _CRT_SECURE_NO_WARNINGS
+)
 
 set(OpenGL_GL_PREFERENCE GLVND)
 find_package(OpenGL REQUIRED)
 target_link_libraries(nanovg OpenGL::GL)
-target_compile_definitions(nanovg PUBLIC NANOVG_GL3_IMPLEMENTATION)
 
 target_include_directories(nanovg SYSTEM PUBLIC "${NANOVG_SOURCE_DIR}")
 
--- cmake/TargetPugl.cmake
+++ cmake/TargetPugl.cmake
@@ -2,10 +2,12 @@ set(PUGL_SOURCE_DIR "${CMAKE_SOURCE_DIR}/extern/pugl")
 
 add_library(pugl STATIC "${PUGL_SOURCE_DIR}/src/implementation.c")
 
-target_compile_options(pugl PRIVATE -O3 -DNDEBUG)
+target_compile_definitions(pugl PUBLIC PUGL_INTERNAL PUGL_STATIC)
 
-target_include_directories(pugl SYSTEM PUBLIC "${PUGL_SOURCE_DIR}/include")
-target_include_directories(pugl SYSTEM PUBLIC "${PUGL_SOURCE_DIR}/bindings/cxx/include")
+target_include_directories(pugl SYSTEM PUBLIC
+	"${PUGL_SOURCE_DIR}/include"
+	"${PUGL_SOURCE_DIR}/bindings/cxx/include"
+)
 
 set(OpenGL_GL_PREFERENCE GLVND)
 find_package(OpenGL REQUIRED)
@@ -21,6 +23,7 @@ elseif (CMAKE_SYSTEM_NAME STREQUAL "Linux")
 	target_link_libraries(pugl OpenGL::GLX)
 elseif (CMAKE_SYSTEM_NAME STREQUAL "Windows")
 	target_sources(pugl PRIVATE "${PUGL_SOURCE_DIR}/src/win.c" "${PUGL_SOURCE_DIR}/src/win_gl.c")
+	target_compile_definitions(pugl PRIVATE WIN32_LEAN_AND_MEAN _CRT_SECURE_NO_WARNINGS)
 else()
 	message(FATAL_ERROR "Unrecognized Platform!")
 endif()
